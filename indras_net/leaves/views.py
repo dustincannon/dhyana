f2b4c47963c25b29f0d3826208a79a5a74b0fa24
Dustin Cannon <dustin.cannon@akimbo.io>
2012-12-19 11:34:03 -0500

    Created leaves/authors view
    * leaves/authors displays a list of authors
    * Created template authors.html
    * Added templates and fixtures for leaves app

--- indras_net/leaves/views.py
+++ indras_net/leaves/views.py
@@ -1,11 +1,22 @@
 # Create your views here.
+from leaves.models import Author
+
 from django.http import HttpResponse
+from django.template import Context, loader
 
 def index(request):
     return HttpResponse("Welcome to the leaves app. Display a random quote here.")
 
 def authors(request):
-    return HttpResponse("Display the list of authors here.")
+
+    authors =  Author.objects.all().order_by('name')
+
+    t = loader.get_template('authors.html')
+    c = Context({
+        'authors': authors,
+    })
+
+    return HttpResponse(t.render(c))
 
 def sources(request):
     return HttpResponse("Display the list of sources here.")
